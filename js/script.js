798b52adde113276260e608a01609cac76267a6f
csslick <digitone@naver.com>
2018-07-26 17:46:42 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/* script.js */
+$(function(){
+	// 2단계 메뉴 닫기
+	$('.depth2').hide();
+	
+	// 메뉴 열고 닫기(hover)
+	$('.depth1 > li').hover(
+		function(){
+			$('.depth2')
+				.stop()
+				.slideDown(200);
+		},
+		function(){
+			$('.depth2')
+				.stop()
+				.slideUp(200);
+		}
+	);
+	
+	// bxslider
+	$('.slider').bxSlider({
+		auto: true,
+		mode: 'fade'
+		
+	});
+	
+	
+	
+});
